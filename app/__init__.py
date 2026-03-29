1a6f6764d2252e425068fcee7ad4433b05787663
jiawei liang <102324738+JayLeung362573@users.noreply.github.com>
2026-03-28 17:25:05 -0700

    Feature/project structure (#10)
    * Initialize Flask project structure
    
    * Add basic home route
    
    * Add Flask dependency
    
    * Load app config and add homepage template

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    return app
