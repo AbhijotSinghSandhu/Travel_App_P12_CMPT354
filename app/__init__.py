4a424626bf97bfa7629e1a3c2fe509759f0f3d0e
Jiawei Liang <jla1028@sfu.ca>
2026-03-28 17:18:26 -0700

    Load app config and add homepage template

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
     @app.route("/")
     def home():
-        return "Travel App is running"
-    
+        return render_template("index.html")
+
     return app
